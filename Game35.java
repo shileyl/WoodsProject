26cb2503d3114997967cf8083c8e9cbee08f1641
Lukas <shileyl@duq.edu>
2021-04-29 23:33:48 -0400

    Game35

--- Game35.java
+++ Game35.java
@@ -1,10 +1,10 @@
-public class Game25 extends Game {
+public class Game35 extends Game {
     
     protected int [] x;
     protected int [] y;
 
     //Game class for grades 2 through 5
-    public Game25(String[] playerNames, String windowName, int boardSizeX, int boardSizeY){
+    public Game35(String[] playerNames, String windowName, int boardSizeX, int boardSizeY){
         super(playerNames, windowName, boardSizeX, boardSizeY);
         for(int i = 0; i < players.length; i++)
             players[i].setLocation((boardSizeX - 1) * (i % 2), (boardSizeY - 1) * (i == 1 || i == 2 ? 1 : 0));
